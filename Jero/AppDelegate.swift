f5f6cd778ce6e0092492b962aa70a9304031a391
Laksh Gandikota <laksh@gandikota.com>
2019-07-25 16:35:51 -0700

    Coredata wiredup with Tableview

--- Jero/AppDelegate.swift
+++ Jero/AppDelegate.swift
@@ -6,19 +6,18 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    var bttableviewcontroller: TableViewController?
+
+    var cdtableviewcontroller: TableViewController?
     lazy var coreDataStack = CoreDataStack(modelName: "Log")
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-       
-        bttableviewcontroller = TableViewController()
-        bttableviewcontroller?.tabBarItem = UITabBarItem(tabBarSystemItem: .recents, tag: 1)
-        bttableviewcontroller?.managedContext = coreDataStack.managedContext
-        
-        
+
+        cdtableviewcontroller = TableViewController()
+        cdtableviewcontroller?.tabBarItem = UITabBarItem(tabBarSystemItem: .recents, tag: 1)
+        cdtableviewcontroller?.managedContext = coreDataStack.managedContext
+
         window = UIWindow()
-        window?.rootViewController = UINavigationController(rootViewController: bttableviewcontroller!)
+        window?.rootViewController = UINavigationController(rootViewController: cdtableviewcontroller!)
         window?.clipsToBounds = true
         window?.makeKeyAndVisible()
 
@@ -27,8 +26,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillResignActive(_ application: UIApplication) {
         print(#function)
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -38,19 +35,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         print(#function)
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         print(#function)
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         print(#function)
         coreDataStack.saveContext()
     }
-
-
+    
 }
 
--- Jero/TableViewController.swift
+++ Jero/TableViewController.swift
@@ -12,6 +12,7 @@ import CoreData
 class TableViewController: UITableViewController {
 
     var managedContext: NSManagedObjectContext!
+    var Allog: [NSManagedObject] = []
     
     enum items: String, CaseIterable {
         case Loading
@@ -28,22 +29,101 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.register(Cell.self, forCellReuseIdentifier: "UITableViewCell")
         tableView.cellLayoutMarginsFollowReadableWidth = false
+        
         navigationItem.title = "Core Data Log"
+        //save(name: "Fourth log test")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+            //appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "AllBackground")
+        
+        do {
+          Allog = try managedContext.fetch(fetchRequest)
+          print(Allog.count)
+        } catch let error as NSError {
+          print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    
+    func save(name: String) {
+
+        let entity = NSEntityDescription.entity(forEntityName: "AllBackground", in: managedContext)!
+        let blog = NSManagedObject(entity: entity, insertInto: managedContext)
+        blog.setValue(name, forKeyPath: "log")
+        blog.setValue(Date(), forKey: "dt")
+        do {
+          try managedContext.save()
+          Allog.append(blog)
+        } catch let error as NSError {
+          print("Could not save. \(error), \(error.userInfo)")
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.allCases.count
+        return Allog.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let alog = Allog[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = alog.value(forKeyPath: "log") as? String
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = items.allCases[indexPath.row].rawValue
-        
+        let insertedDate: Date?
+        insertedDate = alog.value(forKey: "dt") as? Date
+        print(insertedDate?.preciseLocalTime ?? "")
+        cell.detailTextLabel?.text = insertedDate?.preciseLocalTime ?? "NA"
         return cell
         
+    }
+}
+
+
+class Cell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension Formatter {
+    // create static date formatters for your date representations
+    static let preciseLocalTime: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "HH:mm:ss.SSS"
+        return formatter
+    }()
+    
+    static let preciseGMTTime: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.dateFormat = "HH:mm:ss.SSS"
+        return formatter
+    }()
+}
+extension Date {
+    // you can create a read-only computed property to return just the nanoseconds from your date time
+    var nanosecond: Int { return Calendar.current.component(.nanosecond,  from: self)   }
+    // the same for your local time
+    var preciseLocalTime: String {
+        return Formatter.preciseLocalTime.string(for: self) ?? ""
+    }
+    // or GMT time
+    var preciseGMTTime: String {
+        return Formatter.preciseGMTTime.string(for: self) ?? ""
     }
 }
 
